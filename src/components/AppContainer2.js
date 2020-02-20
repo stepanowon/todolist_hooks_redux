ec6c55cba6485a537272d4bb671c67695bf839bc
Hyungseob Won <stepanowon@hotmail.com>
2020-02-21 08:48:57 +0900

    use useCallback Hook with redux codes.

--- src/components/AppContainer2.js
+++ src/components/AppContainer2.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import App from './App';
-import TodoActionCreator from '../redux/TodoActionCreator';
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useCallback } from "react";
+import App from "./App";
+import TodoActionCreator from "../redux/TodoActionCreator";
+import { useDispatch, useSelector } from "react-redux";
 
 const AppContainer = () => {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    var propsObject = {
-        todolist : useSelector(state => state.todolist),
-        addTodo : (todo) => dispatch(TodoActionCreator.addTodo(todo)),
-        deleteTodo : (no) => dispatch(TodoActionCreator.deleteTodo(no)),
-        toggleDone : (no) => dispatch(TodoActionCreator.toggleDone(no)),
-    }
+  var propsObject = {
+    todolist: useSelector(state => state.todolist),
+    addTodo: useCallback(todo => dispatch(TodoActionCreator.addTodo(todo)), [dispatch]),
+    deleteTodo: useCallback(no => dispatch(TodoActionCreator.deleteTodo(no)), [dispatch]),
+    toggleDone: useCallback(no => dispatch(TodoActionCreator.toggleDone(no)), [dispatch])
+  };
 
-    return (
-        <App {...propsObject} />
-    );
+  return <App {...propsObject} />;
 };
 
 export default AppContainer;
